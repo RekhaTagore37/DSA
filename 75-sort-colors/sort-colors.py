1b80dfa5c0fe2b64d40ac759978dabcf4ee7aaee
Rekha Tagore <rekhatagore13@gmail.com>
2026-09-14 21:55:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (91.37%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def sortColors(self, nums):
+        l,mid,r=0,0,len(nums)-1
+        while mid<=r:
+            if nums[mid]==0:
+                nums[l],nums[mid]=nums[mid],nums[l]
+                l+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[r]=nums[r],nums[mid]
+                r-=1
+        return nums
+
+  
+        

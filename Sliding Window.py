aa95089b470b96fc4a6f1c2edba11322999167da
Rekha Tagore <rekhatagore13@gmail.com>
2026-09-16 09:24:10 +0530

    Correct sliding window implementation
    Fix variable name typo and improve sliding window logic.

---
+++ Sliding Window.py
@@ -0,0 +1,8 @@
+class Solution(object):
+  def slidingwindow(self,nums,k):
+    curr_sum=sum(nums[:k])
+    max_sum=curr_sum
+    for i in range(k,len(nums)):
+      curr_sum=currsum-nums[i-k]+nums[i]
+      max_sum=max(max_sum,curr_sum)
+    return max_sum
